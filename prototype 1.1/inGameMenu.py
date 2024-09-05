9745bf6471c3bcb435c844d812d3f77a4de92af5
NoahJack143-V2 <noah.jack.709@gmail.com>
2024-09-04 22:26:48 -0500

    IGM work

--- prototype 1.1/inGameMenu.py
+++ prototype 1.1/inGameMenu.py
@@ -1,7 +1,11 @@
 from threading import Thread
+from time import sleep as s
 import pygame
 
 from settings import *
+
+# initialize pygame
+pygame.init()
 
 # constant variables
 
@@ -18,8 +22,8 @@ class InGameMenu(Thread):
         self.__selection = 1
         # the list will look like this:
         # [ Pokemon , Pokedex,		[ 1 , 2,
-        #   Bag , Success,			[ 3 , 4,
-        #   Save , Options]			[ 5 , 6]
+        #   Bag , Success,			  3 , 4,
+        #   Save , Options ]		  5 , 6 ]
   
     def run(self) -> None: # Thread.start() starts this
         # this is going to be the main thread
@@ -27,4 +31,22 @@ class InGameMenu(Thread):
         while not self.quit:
             
             # check to see if the user presses W, A, S, or D, and change the value of __selection accordingly
-            if pygame.key.get_pressed()[pygame.K_w]: print("w")
+            if pygame.key.get_pressed()[pygame.K_w]:
+                print("w")
+                
+                while pygame.key.get_pressed()[pygame.K_w]: pass
+                
+            elif pygame.key.get_pressed()[pygame.K_a]:
+                print("a")
+                
+                while pygame.key.get_pressed()[pygame.K_a]: pass
+                
+            elif pygame.key.get_pressed()[pygame.K_s]:
+                print("s")
+                
+                while pygame.key.get_pressed()[pygame.K_s]: pass
+                
+            elif pygame.key.get_pressed()[pygame.K_d]:
+                print("d")
+                
+                while pygame.key.get_pressed()[pygame.K_d]: pass
